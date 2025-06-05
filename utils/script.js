ec7c48db4b572e0065b4c90263f5c68799f5816c
StudioBellota <studiobellota.art@gmail.com>
2025-06-04 20:50:28 -0500

    musica js

--- utils/script.js
+++ utils/script.js
@@ -10,3 +10,8 @@ window.addEventListener('resize', function() {
         document.body.style.overflow = '';
     }, 300);
 });
+
+window.addEventListener('DOMContentLoaded', () => {
+    const audio = document.getElementById('bgMusic');
+    audio.volume = 0.15;
+});
